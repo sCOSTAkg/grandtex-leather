45ff568ecf6deabae21c7402a4ea3b919cdc9671
sCOSTAkg <abakirsallahadin@gmail.com>
2025-08-29 08:39:28 +0600

    Enable image accessibility lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,8 +21,6 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off",
-      "jsx-a11y/alt-text": "off",
     },
   },
 ];
