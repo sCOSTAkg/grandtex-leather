78e5fa4070055ef11dc472e9abaf6e746cca6876
sCOSTAkg <abakirsallahadin@gmail.com>
2025-08-29 09:08:44 +0600

    feat: allow additional remote image sources

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     domains: [
       'images.unsplash.com',
       'source.unsplash.com',
+      'ext.same-assets.com',
+      'ugc.same-assets.com',
+      'picsum.photos',
     ],
     remotePatterns: [
       {
@@ -15,6 +18,21 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'ext.same-assets.com',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'ugc.same-assets.com',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
         pathname: '/**'
       }
     ]
