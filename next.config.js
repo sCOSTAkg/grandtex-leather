13db5e914d695509598c227be49e9e9be8d4473c
sCOSTAkg <abakirsallahadin@gmail.com>
2025-08-30 12:08:40 +0600

    chore: sort remote image host lists

--- next.config.js
+++ next.config.js
@@ -3,20 +3,38 @@ const nextConfig = {
   images: {
     unoptimized: true,
     domains: [
+      'ext.same-assets.com',
       'images.unsplash.com',
+      'picsum.photos',
       'source.unsplash.com',
+      'ugc.same-assets.com',
     ],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'source.unsplash.com',
-        pathname: '/**'
+        hostname: 'ext.same-assets.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        pathname: '/**'
-      }
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ugc.same-assets.com',
+        pathname: '/**',
+      },
     ]
   },
 };
